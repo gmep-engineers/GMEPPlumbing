751ddaa688b7059d4ab318a60d0ee20e7fac294f
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-05-27 10:41:33 -0700

    database maria

--- GMEPPlumbing/Properties/Settings.Designer.cs
+++ GMEPPlumbing/Properties/Settings.Designer.cs
@@ -8,22 +8,31 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GMEPPlumbing.Properties
-{
+namespace GMEPPlumbing.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
-        public static Settings Default
-        {
-            get
-            {
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=gmep-design-tool-test.ch8c88cauy2x.us-west-1.rds.amazonaws.com;Port=3306;D" +
+            "atabase=gmep-design-tool;User Id=admin;Password=f51Vu3o5Qlc140r;")]
+        public string ConnectionString {
+            get {
+                return ((string)(this["ConnectionString"]));
+            }
+            set {
+                this["ConnectionString"] = value;
             }
         }
     }
---
+++ GMEPPlumbing/Services/MariaDBService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GMEPPlumbing.Services
+{
+    public class MariaDBService
+    {
+        public string ConnectionString { get; set; }
+        public MySqlConnection Connection { get; set; }
+
+        public MariaDBService()
+        {
+            ConnectionString = connectionString;
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
