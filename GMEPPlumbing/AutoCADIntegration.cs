5f4c7668f9a15fe32863caec24ebe6811d0709ba
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-05-29 09:58:27 -0700

    setting plumbing base point

--- GMEPPlumbing/AutoCADIntegration.cs
+++ GMEPPlumbing/AutoCADIntegration.cs
@@ -13,6 +13,8 @@ using System.Collections;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms.Integration;
+using System.Windows.Media.Media3D;
+using Autodesk.AutoCAD.Geometry;
 
 [assembly: CommandClass(typeof(GMEPPlumbing.AutoCADIntegration))]
 [assembly: CommandClass(typeof(GMEPPlumbing.Commands.TableCommand))]
@@ -35,13 +37,80 @@ namespace GMEPPlumbing
     public Database db { get; private set; }
     public Editor ed { get; private set; }
     public string ProjectId { get; private set; } = string.Empty;
+
     [CommandMethod("SETPLUMBINGBASEPOINT")]
     public async void setPlumbingBasePoint()
     {
         doc = Application.DocumentManager.MdiActiveDocument;
         db = doc.Database;
         ed = doc.Editor;
+        PromptKeywordOptions viewportOptions = new PromptKeywordOptions("Enter the Viewport Type");
+        viewportOptions.Keywords.Add("COLD-HOT WATER");
+        viewportOptions.Keywords.Add("GAS");
+        viewportOptions.Keywords.Add("WATER-GAS");
+        viewportOptions.Keywords.Add("SEWER");
+        viewportOptions.Keywords.Add("STORM");
+        viewportOptions.Keywords.Add("SEWER-STORM");
 
+        PromptStringOptions planNameOption = new PromptStringOptions("\nEnter the plan name: ");
+        PromptStringOptions floorQtyOption = new PromptStringOptions("\nEnter the floor quantity: ");
+        
+        PromptResult viewportResult = ed.GetKeywords(viewportOptions);
+        string viewport = viewportResult.StringResult.ToUpper();
+
+        PromptResult planeNameResult = ed.GetString(planNameOption);
+        string planName = planeNameResult.StringResult;
+
+        PromptResult floorQtyResult = ed.GetString(floorQtyOption);
+
+        if (!int.TryParse(floorQtyResult.StringResult, out int floorQty))
+        {
+            ed.WriteMessage("\nInvalid floor quantity. Please enter a valid integer.");
+            return;
+        }
+        if (viewportResult.Status == PromptStatus.OK && planeNameResult.Status == PromptStatus.OK && floorQtyResult.Status == PromptStatus.OK)
+        {
+            for (int i = 0; i < floorQty; i++)
+            {
+                Point3d point;
+                ObjectId blockId;
+                using (Transaction tr = db.TransactionManager.StartTransaction())
+                {
+                    BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForRead);
+                    BlockTableRecord curSpace = (BlockTableRecord)tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite);
+
+
+                    BlockTableRecord block;
+                    string message = "\nCreating Plumbing Base Point for " + planName + " on floor " + (i + 1);
+                    BlockReference br = CADObjectCommands.CreateBlockReference(
+                    tr,
+                    bt,
+                    "GMEP PLMG Basepoint",
+                    out block,
+                    out point
+                    );
+                    if (br != null)
+                    {
+                        curSpace.AppendEntity(br);
+                        tr.AddNewlyCreatedDBObject(br, true);
+                        blockId = br.ObjectId;
+                        DynamicBlockReferencePropertyCollection properties = br.DynamicBlockReferencePropertyCollection;
+                        foreach (DynamicBlockReferenceProperty prop in properties)
+                        {
+                            if (prop.PropertyName == "Plan")
+                            {
+                                prop.Value = planName;
+                            }
+                            else if (prop.PropertyName == "Floor")
+                            {
+                                prop.Value = i + 1;
+                            }
+                        }
+                    }
+                    tr.Commit();
+                }
+            }
+        }
     }
 
     [CommandMethod("Water")]
--- GMEPPlumbing/Tools/AutoCADTools.cs
+++ GMEPPlumbing/Tools/AutoCADTools.cs
@@ -28,5 +28,30 @@ namespace GMEPPlumbing
             return Regex.Match(fileName, @"[0-9]{2}-[0-9]{3}").Value;
         }
 
+        public static BlockReference CreateBlockReference(Transaction tr, BlockTable bt, string blockName, out BlockTableRecord block, out Point3d point)
+        {
+            if (!bt.Has(blockName))
+            {
+                Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog(
+                  $"Block '{blockName}' not found in the BlockTable."
+                );
+                block = null;
+                point = Point3d.Origin;
+                return null;
+            }
+            block = (BlockTableRecord)
+                    tr.GetObject(bt[blockName], OpenMode.ForRead);
+
+            BlockJig blockJig = new BlockJig();
+            PromptResult res = blockJig.DragMe(block.ObjectId, out point);
+            if (res.Status != PromptStatus.OK)
+            {
+                return null;
+            }
+            BlockReference br = new BlockReference(point, block.ObjectId);
+            return br;
+        }
+
+
     }
 }
