cc015a44c71ebf66b86d9943e9e0907ac8a0dd08
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-06-09 14:11:13 -0700

    meow

---
+++ GMEPPlumbing/Models/HorizontalRouteLine.cs
@@ -0,0 +1,57 @@
+﻿using Autodesk.AutoCAD.Geometry;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMEPPlumbing.Models
+{
+    class HorizontalRouteLine
+    {
+        private string id;
+        private Point3d startPoint;
+        private Point3d endPoint;
+        private int typeId;
+        private string projectId;
+        public HorizontalRouteLine(string id, Point3d startPoint, Point3d endPoint, int typeId, string projectId)
+        {
+            this.id = id;
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.typeId = typeId;
+            this.projectId = projectId;
+        }
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public Point3d StartPoint
+        {
+            get { return startPoint; }
+            set { startPoint = value; }
+        }
+        public Point3d EndPoint
+        {
+            get { return endPoint; }
+            set { endPoint = value; }
+        }
+        public int TypeId
+        {
+            get { return typeId; }
+            set { typeId = value; }
+        }
+        public string ProjectId
+        {
+            get { return projectId; }
+            set { projectId = value; }
+        }
+        public override string ToString()
+        {
+            return $"HorizontalRouteLine: {id}, Start: {startPoint}, End: {endPoint}, TypeId: {typeId}, ProjectId: {projectId}";
+        }
+
+    }
+}
---
+++ GMEPPlumbing/Models/VerticalRoute.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.AutoCAD.MacroRecorder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMEPPlumbing.Models
+{
+    class VerticalRoute
+    {
+        private string id;
+        private string projectId;
+        private int startFloor;
+        private int endFloor;
+        private int typeId;
+        public VerticalRoute(string id, string projectId, int startFloor, int endFloor, int typeId)
+        {
+            this.id = id;
+            this.projectId = projectId;
+            this.startFloor = startFloor;
+            this.endFloor = endFloor;
+            this.typeId = typeId;
+        }
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string ProjectId
+        {
+            get { return projectId; }
+            set { projectId = value; }
+        }
+        public int StartFloor
+        {
+            get { return startFloor; }
+            set { startFloor = value; }
+        }
+        public int EndFloor
+        {
+            get { return endFloor; }
+            set { endFloor = value; }
+        }
+        public int TypeId
+        {
+            get { return typeId; }
+            set { typeId = value; }
+        }
+        public override string ToString()
+        {
+            return $"VerticalRoute: {id}, ProjectId: {projectId}, StartFloor: {startFloor}, EndFloor: {endFloor}, TypeId: {typeId}";
+        }
+
+
+    }
+}
