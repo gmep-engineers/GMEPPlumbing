f2dce90f9436870c40bae391c4e46c224f614df3
Ethan Stafford <ethanstafford.dev@gmail.com>
2025-06-24 11:24:21 -0700

    normalizing routes

--- GMEPPlumbing/Views/Routing.xaml.cs
+++ GMEPPlumbing/Views/Routing.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GMEPPlumbing.Services;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -13,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Autodesk.AutoCAD.Geometry;
+using System.Windows.Media.Media3D;
 
 namespace GMEPPlumbing.Views
 {
@@ -22,11 +25,46 @@ namespace GMEPPlumbing.Views
     public partial class Routing : UserControl
     {
       public Dictionary<string, List<PlumbingFullRoute>> FullRoutes { get; set; } = new Dictionary<string, List<PlumbingFullRoute>>();
-      public Routing(Dictionary<string, List<PlumbingFullRoute>> fullRoutes)
+
+      public Dictionary<string, PlumbingPlanBasePoint> BasePointLookup { get; set; } = new Dictionary<string, PlumbingPlanBasePoint>();
+      public Routing(Dictionary<string, List<PlumbingFullRoute>> fullRoutes, Dictionary<string, PlumbingPlanBasePoint> basePointLookup)
       {
         FullRoutes = fullRoutes;
+        BasePointLookup = basePointLookup;
+        NormalizeRoutes();
         InitializeComponent();
         DataContext = this;
+      }
+      public void NormalizeRoutes() {
+        foreach (var fullRoute in FullRoutes.Values) {
+          foreach (var route in fullRoute) {
+            foreach (var item in route.RouteItems) {
+              if (item is PlumbingHorizontalRoute horizontalRoute) {
+                if (BasePointLookup.TryGetValue(horizontalRoute.BasePointId, out var basePoint)) {
+                  horizontalRoute.StartPoint = new Point3d(
+                    horizontalRoute.StartPoint.X - basePoint.Point.X,
+                    horizontalRoute.StartPoint.Y - basePoint.Point.Y,
+                    horizontalRoute.StartPoint.Z
+                  );
+                  horizontalRoute.EndPoint = new Point3d(
+                    horizontalRoute.EndPoint.X - basePoint.Point.X,
+                    horizontalRoute.EndPoint.Y - basePoint.Point.Y,
+                    horizontalRoute.EndPoint.Z
+                  );
+                }
+              }
+              else if (item is PlumbingVerticalRoute verticalRoute) {
+                if (BasePointLookup.TryGetValue(verticalRoute.BasePointId, out var basePoint)) {
+                  verticalRoute.Position = new Point3d(
+                    verticalRoute.Position.X - basePoint.Point.X,
+                    verticalRoute.Position.Y - basePoint.Point.Y,
+                    verticalRoute.Position.Z
+                  );
+                }
+              }
+            }
+          }
+        }
       }
     }
     public class InchesToFeetInchesConverter : IValueConverter {
@@ -43,4 +81,5 @@ namespace GMEPPlumbing.Views
         throw new NotImplementedException();
       }
     }
+
 }
